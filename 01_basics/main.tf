c2de74b4d1b429ad43d7008c499fcd9c96d1eacd
Ikanyeng Adams <ikanyengadams2@gmail.com>
2026-04-01 11:13:57 +0200

    learned about the difference between a local sensitive file and a local file

--- 01_basics/main.tf
+++ 01_basics/main.tf
@@ -1,7 +1,13 @@
 # simple file resource
 
 resource "local_file" "tf_example1" {
-#   filename = "01_basics/example.txt"
-    filename = "C:\\Users\\Goldman\\Downloads\\Terraform-cours\\01_basics\\example1.txt"
-    content = "Hey, this is the terraform course"
+  filename = "./example1.txt"
+    # filename = "C:\\Users\\Goldman\\Downloads\\Terraform-cours\\01_basics\\example1.txt"
+    content = "Ika is a good student!"
+}
+
+
+resource "local_sensitive_file" "tf_example2" {
+  content  = "This is a sensiitive file!"
+  filename = "${path.module}/sensitive.txt"
 }
